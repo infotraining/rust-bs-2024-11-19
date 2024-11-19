f7b80d276b2a2889df6bcc0c441cf7b1796f75cc
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-11-20 00:16:13 +0100

    After 1st day

--- code/hello-world/src/main.rs
+++ code/hello-world/src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
-    let mut message = "Hello, world!".to_string();
-    message += "!!";
-    println!("{message}");
+    println!("Hello, world!");
+
+    let x: u8 = 255;
+    let y = x.wrapping_add(1);
+
+    println!("x: {}", x);
+    println!("y: {}", y);
+
+    let z = x.checked_add(1);
+    match z {
+        Some(value) => println!("z: {}", value),
+        None => println!("overflow"),
+    }
+
+    let a = x.saturating_add(1);
+    println!("a: {}", a);
 }
