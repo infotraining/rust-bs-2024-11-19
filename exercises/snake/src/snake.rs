bb3168cedbee7b17199fe05fd850b0f688fa9997
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-11-22 15:00:12 +0100

    Snake - move up

--- exercises/snake/src/snake.rs
+++ exercises/snake/src/snake.rs
@@ -7,10 +7,15 @@
  #[derive(Debug, PartialEq)]
  enum Direction {
     Up,
+    Down,
+    Left, 
+    Right
  }
  
+ #[derive(Debug, PartialEq)]
  struct Snake { 
-    points: Vec<Point>
+    points: Vec<Point>,
+    direction: Direction
  }
 
  impl Snake {
@@ -19,7 +24,7 @@
         for point in initial_points {
             points.push(Point{x: point.0, y: point.1});
         }
-        Snake{points}
+        Snake{points, direction: Direction::Up}
     }
        
     fn head(&self) -> &Point {
@@ -28,6 +33,24 @@
 
     fn direction(&self) -> Direction {
         Direction::Up
+    }
+    
+    fn set_direction(&mut self, new_direction: Direction) {
+        self.direction = new_direction;
+    }
+
+    fn next(&mut self) {
+        let Point{x, y} = *self.head();
+
+        let new_head = match self.direction {
+            Direction::Up => Point{x: x, y: y - 1},
+            Direction::Down => Point{x: x, y: y + 1},
+            Direction::Left => Point{x: x - 1, y: y},
+            Direction::Right => Point{x: x + 1, y: y}
+        };
+
+        self.points.insert(0, new_head);
+        self.points.pop();
     }
  }
 
@@ -43,4 +66,12 @@ fn snake_has_default_direction_up() {
     let snake = Snake::new(vec![(0, 0), (1, 0), (2, 0)]);
 
     assert_eq!(snake.direction(), Direction::Up);
+}
+
+#[test]
+fn snake_moves_in_given_direction() {
+    let mut snake = Snake::new(vec![(5, 5), (5, 6), (5, 7)]);
+    snake.set_direction(Direction::Up);
+    snake.next();
+    assert_eq!(snake, Snake::new(vec![(5, 4), (5, 5), (5, 6)]));
 }
